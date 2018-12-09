3440bf370d5cda2abc97e415161ada6cc24d6352
unknown <mariana.costa214@gmail.com>
2018-12-09 23:23:59 +0000

    hmm não sei o que fazer :)

---
+++ Project-2/src/downloadApp.h
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include <netdb.h>
+#include <strings.h>
+
+#define SERVER_PORT 6000
+#define SERVER_ADDR "192.168.28.96"
+
+int main(int argc, char** argv);
+void parseURL();
