4a6406aa13c54f1f03de192f648b602c3559fa0c
unknown <mariana.costa214@gmail.com>
2018-12-11 11:16:49 +0000

    added info file about ftp

--- Project-2/src/downloadApp.c
+++ Project-2/src/downloadApp.c
@@ -29,7 +29,7 @@ int main(int argc, char** argv) {
 	server_addr.sin_addr.s_addr = inet_addr(SERVER_ADDR);	/*32 bit Internet address network byte ordered*/
 	server_addr.sin_port = htons(SERVER_PORT);		/*server TCP port must be network byte ordered */
     
-	/*open an TCP socket*/
+	/*open a TCP socket*/
 	if ((sockfd = socket(AF_INET,SOCK_STREAM,0)) < 0) {
     		perror("socket()");
         	exit(0);
