1700261695e592658c30c6392aff6cce74e5d068
Tiago Castro <tiagoaraujocastro@gmail.com>
2018-12-11 12:29:48 +0000

    parseInfo

--- Project-2/src/downloadApp.c
+++ Project-2/src/downloadApp.c
@@ -9,7 +9,7 @@
 #include <netdb.h>
 #include <strings.h>
 
-#define SERVER_PORT 6000
+#define SERVER_PORT 21
 #define SERVER_ADDR "192.168.28.96"
 
 int main(int argc, char** argv) {
@@ -20,23 +20,23 @@ int main(int argc, char** argv) {
 
 	int	sockfd;
 	struct	sockaddr_in server_addr;
-	char	buf[] = "Mensagem de teste na travessia da pilha TCP/IP\n";  
+	char	buf[] = "Mensagem de teste na travessia da pilha TCP/IP\n";
 	int	bytes;
-	
+
 	/*server address handling*/
 	bzero((char*)&server_addr,sizeof(server_addr));
 	server_addr.sin_family = AF_INET;
 	server_addr.sin_addr.s_addr = inet_addr(SERVER_ADDR);	/*32 bit Internet address network byte ordered*/
 	server_addr.sin_port = htons(SERVER_PORT);		/*server TCP port must be network byte ordered */
-    
+
 	/*open a TCP socket*/
 	if ((sockfd = socket(AF_INET,SOCK_STREAM,0)) < 0) {
     		perror("socket()");
         	exit(0);
     	}
 	/*connect to the server*/
-  if(connect(sockfd, 
-            (struct sockaddr *)&server_addr, 
+  if(connect(sockfd,
+            (struct sockaddr *)&server_addr,
             sizeof(server_addr)) < 0) {
     perror("connect()");
     exit(0);
@@ -52,9 +52,9 @@ int main(int argc, char** argv) {
   * IP
   */
 
-  struct hostent *h;       
-        
-  if ((h=gethostbyname(argv[1])) == NULL) {  
+  struct hostent *h;
+
+  if ((h=gethostbyname(argv[1])) == NULL) {
     herror("gethostbyname");
     exit(1);
   }
@@ -65,4 +65,55 @@ int main(int argc, char** argv) {
   return 0;
 }
 
+void parseInfo(char* info, char* user, char* password, char* host, char* path){
+	unsigned int i = 6;
+	unsigned int j = 0;
+	unsigned int length = strlen(info);
+	unsigned int state = 0;
+	unsigned char buf;
 
+	while(i<length){
+		buf = *(buf+i);
+
+		switch(state){
+			case 0:
+				if(buf == ':'){
+					*(user + j) = 0; //end char
+					state++;
+					j = 0;
+				}else{
+					*(user + j) = buf;
+					j++;
+				}
+				break;
+			case 1:
+				if(buf == '@'){
+					*(password + j) = 0; //end char
+					state++;
+					j = 0;
+				}else{
+					*(password + j) = buf;
+					j++;
+				}
+				break;
+			case 2:
+				if(buf == '/'){
+					*(host + j) = 0; //end char
+					state++;
+					j = 0;
+				}else{
+					*(host + j) = buf;
+					j++;
+				}
+				break;
+			case 3:
+				*(path + j) = buf;
+				j++;
+				break;
+		}
+
+		*(path + j) = 0; //end char
+
+		i++;
+	}
+}
