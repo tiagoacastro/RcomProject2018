f3ae59201374695c591cadefee0f15a00ab906b2
Tiago Castro <tiagoaraujocastro@gmail.com>
2018-12-13 13:20:41 +0000

    made parsefile and ordered code

--- Project-2/src/downloadApp.c
+++ Project-2/src/downloadApp.c
@@ -9,49 +9,37 @@
 #include <netdb.h>
 #include <strings.h>
 
-#define SERVER_PORT 21
-#define SERVER_ADDR "192.168.28.96"
+#define SERVER_PORT 				21
+#define SERVER_ADDR 				"192.168.28.96"
+#define MAX_STRING_LENGTH		64
 
 int main(int argc, char** argv) {
-
-  /*
-  * TCP
-  */
-
 	int	sockfd;
 	struct	sockaddr_in server_addr;
-	char	buf[] = "Mensagem de teste na travessia da pilha TCP/IP\n";
-	int	bytes;
+	struct hostent *h;
 
-	/*server address handling*/
-	bzero((char*)&server_addr,sizeof(server_addr));
-	server_addr.sin_family = AF_INET;
-	server_addr.sin_addr.s_addr = inet_addr(SERVER_ADDR);	/*32 bit Internet address network byte ordered*/
-	server_addr.sin_port = htons(SERVER_PORT);		/*server TCP port must be network byte ordered */
+	char user[MAX_STRING_LENGTH];
+	char password[MAX_STRING_LENGTH];
+	char host[MAX_STRING_LENGTH];
+	char path[MAX_STRING_LENGTH];
 
-	/*open a TCP socket*/
-	if ((sockfd = socket(AF_INET,SOCK_STREAM,0)) < 0) {
-    		perror("socket()");
-        	exit(0);
-    	}
-	/*connect to the server*/
-  if(connect(sockfd,
-            (struct sockaddr *)&server_addr,
-            sizeof(server_addr)) < 0) {
-    perror("connect()");
-    exit(0);
-	}
+	parseInfo(argv[1], user, password, host, path);
+
+	char file[MAX_STRING_LENGTH];
+
+	parseFile(path, file);
+
+	h = getip(host);
+
+	sockfd = openTCPPort(server_addr);
+
+	//continue here
 
 	close(sockfd);
   return 0;
 }
 
-void parseArguments(char * user, char * pass, char * host, char * url) {
-
-}
-
 int openTCPPort(struct	sockaddr_in server_addr) {
-
   /*server address handling*/
 	bzero((char*)&server_addr,sizeof(server_addr));
 	server_addr.sin_family = AF_INET;
@@ -75,9 +63,9 @@ int openTCPPort(struct	sockaddr_in server_addr) {
   return sockfd;
 }
 
-void getIp(char * address) {
+void getIp(char* address) {
 
-  struct hostent *h;             
+  struct hostent* h;             
   if ((h=gethostbyname(address)) == NULL) {  
     herror("gethostbyname");
     exit(1);
@@ -86,14 +74,7 @@ void getIp(char * address) {
   printf("Host name  : %s\n", h->h_name);
   printf("IP Address : %s\n",inet_ntoa(*((struct in_addr *)h->h_addr)));
 
-  return 0;
-}
-
-int sendCommand(int socketfd) {
-  char buf[] = "Mensagem de teste na travessia da pilha TCP/IP\n";
-  bytes = write(sockfd, buf, strlen(buf));
-	printf("Bytes escritos %d\n", bytes);
-  return bytes;
+  return h;
 }
 
 void parseInfo(char* info, char* user, char* password, char* host, char* path){
@@ -104,7 +85,7 @@ void parseInfo(char* info, char* user, char* password, char* host, char* path){
 	unsigned char buf;
 
 	while(i<length){
-		buf = *(buf+i);
+		buf = *(info+i);
 
 		switch(state){
 			case 0:
@@ -145,6 +126,27 @@ void parseInfo(char* info, char* user, char* password, char* host, char* path){
 
 		*(path + j) = 0; //end char
 
+		i++;
+	}
+}
+
+void parseFile(char* path, char* file){
+	unsigned int length = strlen(path);
+	unsigned int i = 0;
+	unsigned int j = 0;
+
+	while(i <= length){
+		if(*(path + i) == 0){
+			*(file + j) = 0; //end char;
+			break;
+		}else{
+			if(*(path + i) == '/'){
+				j = 0;
+			}else{
+				*(file + j) = *(path + i);
+				j++;
+			}
+		}
 		i++;
 	}
 }
